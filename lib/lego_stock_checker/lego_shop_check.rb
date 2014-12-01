4ad0b4008f0d9b05e95695699d3b6e33baa381d8
Taylor Mock <tmock12@gmail.com>
2014-12-01 13:06:54 -0500

    Add lego shop check specs

--- lib/lego_stock_checker/lego_shop_check.rb
+++ lib/lego_stock_checker/lego_shop_check.rb
@@ -3,7 +3,7 @@ require 'lego_stock_checker/base_check'
 module LegoStockChecker
   class LegoShopCheck < BaseCheck
     def in_stock?
-      !page.at_css("li[class*='availability']").text.match(/sold out|out of stock/i)
+      !page.at_css("li[class*='availability']").text.match(/sold out|out of stock|call to check/i)
     end
   end
 end
---
+++ spec/lego_stock_checker/lego_shop_check_spec.rb
@@ -0,0 +1,27 @@
+require 'lego_stock_checker/lego_shop_check'
+require 'shared_examples_for_lego_stock_checkers'
+
+describe LegoStockChecker::LegoShopCheck do
+
+  context 'when in stock' do
+    describe '#in_stock?' do
+      let(:html_file) { 'lego-shop-is.html' }
+      it_behaves_like "in stock"
+    end
+  end
+
+  context 'when call to check stock' do
+    describe "#in_stock?" do
+      let(:html_file) { 'lego-shop-call-to-check.html' }
+      it_behaves_like "out of stock"
+    end
+  end
+
+  context 'when out of stock' do
+    describe "#in_stock?" do
+      let(:html_file) { 'lego-shop-oos.html' }
+      it_behaves_like "out of stock"
+    end
+  end
+
+end
