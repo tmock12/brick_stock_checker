71d9679b6567c91a8318409183df41a189c71602
Taylor Mock <tmock12@gmail.com>
2014-12-02 09:35:10 -0500

    Add tru check specs

---
+++ spec/lego_stock_checker/tru_check_spec.rb
@@ -0,0 +1,20 @@
+require 'lego_stock_checker/tru_check'
+require 'shared_examples_for_lego_stock_checkers'
+
+describe LegoStockChecker::TruCheck do
+
+  context 'when in stock' do
+    describe '#in_stock?' do
+      let(:html_file) { 'tru-is.html' }
+      it_behaves_like "in stock"
+    end
+  end
+
+  context 'when out of stock' do
+    describe "#in_stock?" do
+      let(:html_file) { 'tru-oos.html' }
+      it_behaves_like "out of stock"
+    end
+  end
+
+end
