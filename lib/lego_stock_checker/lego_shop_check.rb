7d086518ed21607dfe2d122b0ef5f43702500857
Taylor Mock <tmock12@gmail.com>
2014-12-04 22:42:19 -0500

    Retired products marked as OOS for Lego SAH

--- lib/lego_stock_checker/lego_shop_check.rb
+++ lib/lego_stock_checker/lego_shop_check.rb
@@ -3,7 +3,7 @@ require 'lego_stock_checker/base_check'
 module LegoStockChecker
   class LegoShopCheck < BaseCheck
     def in_stock?
-      !page.at_css("li[class*='availability']").text.match(/sold out|out of stock|call to check/i)
+      !page.at_css("li[class*='availability']").text.match(/retired|sold out|out of stock|call to check/i)
     end
 
     def price
